7fe90088bacc6e9eba48d46da41f91a44b0a62e4
vikaskumar02 <vikas.kumar02@nagarro.com>
2023-03-26 02:05:11 -0400

    Fixed bug of not throwing exception in app if any nutrient is not available in the response.

--- FoodAnalyzer/FoodAnalyzer/FoodNutrition.cs
+++ FoodAnalyzer/FoodAnalyzer/FoodNutrition.cs
@@ -7,25 +7,25 @@ namespace FoodAnalyzer
     {
         public string? Food_Name { get; set; } = string.Empty;
         [Display(Name = "Total Calories")]
-        public double Nf_Calories { get; set; }
+        public double? Nf_Calories { get; set; }
         [Display(Name = "Total Fat")]
-        public double Nf_Total_Fat { get; set; }
+        public double? Nf_Total_Fat { get; set; }
         [Display(Name = "Saturated Fat")]
-        public double Nf_Saturated_Fat { get; set; }
+        public double? Nf_Saturated_Fat { get; set; }
         [Display(Name = "Cholesterol")]
-        public double Nf_Cholesterol { get; set; }
+        public double? Nf_Cholesterol { get; set; }
         [Display(Name = "Sodium")]
-        public double Nf_Sodium { get; set; }
+        public double? Nf_Sodium { get; set; }
         [Display(Name = "Carbohydrate")]
-        public double Nf_Total_Carbohydrate { get; set; }
+        public double? Nf_Total_Carbohydrate { get; set; }
         [Display(Name = "Fiber")]
-        public double Nf_Dietary_Fiber { get; set; }
+        public double? Nf_Dietary_Fiber { get; set; }
         [Display(Name = "Sugar")]
-        public double Nf_Sugars { get; set; }
+        public double? Nf_Sugars { get; set; }
         [Display(Name = "Protein")]
-        public double Nf_Protein { get; set; }
+        public double? Nf_Protein { get; set; }
         [Display(Name = "Potassium")]
-        public double Nf_Potassium { get; set; }
+        public double? Nf_Potassium { get; set; }
 
     }
 }
